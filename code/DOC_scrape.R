aad902c8f2efcf750e4b11632bdfd0575e36cc18
everetr <2319774-everetr@users.noreply.gitlab.com>
2020-04-19 15:58:07 -0400

    Validate timestamps. Reduce unnecessary function calls.

--- code/DOC_scrape.R
+++ code/DOC_scrape.R
@@ -1,15 +1,15 @@
-library(dplyr)
-library(glue)
-library(purrr)
-library(readr)
-library(rvest)
-library(stringr)
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(glue)
+  library(purrr)
+  library(readr)
+  library(rvest)
+  library(stringr)
+})
 
 src = read_html("https://njdoc.gov/pages/COVID19Updates.shtml")
-
-tables = src %>%
-  html_nodes(css = ".align-text-top .table-striped") %>%
-  html_table()
 
 timestamps = src %>%
   html_nodes(css = ".align-text-top p") %>%
@@ -17,15 +17,28 @@ timestamps = src %>%
   str_extract("\\d+/\\d+/\\d{4} \\d+:\\d+:\\d+ (AM|PM)") %>%
   as.POSIXct(format = "%m/%d/%Y %I:%M:%S %p")
 
-data_out = map2_dfr(.x = tables, .y = timestamps,
-                    ~{
-                      .x %>%
-                        mutate(as_of = .y,
-                               system = names(.)[1]) %>%
-                        rename(location = 1) %>%
-                        rename_all(tolower) %>%
-                        select(as_of, system, location, everything())
-                    })
+# Prison and halfway house datasets each have a timestamp. They _should_ be the
+# same, but we'll notice if they ever differ.
+if (timestamps[1] != timestamps[2]) {
+  stop("Prison and halfway house timestamps differ.")
+} else {
+  timestamp_ = timestamps[1]
+}
 
-datestamp = strftime(timestamps[1], "%Y-%m-%d")
+tables = src %>%
+  html_nodes(css = ".align-text-top .table-striped") %>%
+  html_table()
+
+data_out =
+  map_dfr(.x = tables,
+          ~{
+            .x %>%
+              mutate(as_of = timestamp_,
+                     system = names(.)[1]) %>%
+              rename(location = 1)
+          }) %>%
+  rename_all(tolower) %>%
+  select(as_of, system, location, everything())
+
+datestamp = strftime(timestamp_, "%Y-%m-%d")
 write_csv(data_out, glue("data/DOC/NJ_DOC_COVID-19-Updates_{datestamp}.csv"))
