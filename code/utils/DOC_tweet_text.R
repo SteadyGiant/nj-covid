e6ea48fc2bbada5fc5b8f93c47e12401da2f9b24
everetr <2319774-everetr@users.noreply.gitlab.com>
2020-06-08 11:40:54 -0400

    No more testing in tweets

--- code/utils/DOC_tweet_text.R
+++ code/utils/DOC_tweet_text.R
@@ -14,12 +14,6 @@ DOC_tweet_text = function(locations_data, testing_data) {
   # Cannot allow ANY indentation beyond first line
   TWEET_PLAN = "{mdd}
 
-All Inmates
-Tested: {tested} (+{tested_diff})
-Positive: {cases_total} (+{cases_total_diff})
-Negative: {negative}
-Pending: {pending}
-
 Prisons
 Cases: {cases_prisons} (+{cases_prisons_diff})
 Deaths: {deaths_prisons} (+{deaths_prisons_diff})
@@ -36,16 +30,8 @@ Data: {DATA_URL}
 
   mdd =
     max(locations_data$as_of) %>%
-    strftime("%m/%d") %>%
+    format("%m/%d", tz = "EST") %>%
     substr(2, nchar(.))
-
-  test_new = testing_data[which.max(testing_data$as_of), ]
-  test_old = testing_data[which.min(testing_data$as_of), ]
-
-  tested      = test_new$tested
-  cases_total = test_new$positive
-  negative    = test_new$negative
-  pending     = test_new$pending
 
   # Because which.m** return only the first value
   locs_new = locations_data[locations_data$as_of == max(locations_data$as_of), ]
@@ -65,8 +51,6 @@ Data: {DATA_URL}
   # staffed by non-DOC employees
   cases_employees = prisons_new$employees
 
-  tested_diff          = tested - test_old$tested
-  cases_total_diff     = cases_total - test_old$positive
   cases_prisons_diff   = cases_prisons - prisons_old$inmates
   deaths_prisons_diff  = deaths_prisons - prisons_old$inmate_deaths
   cases_halfway_diff   = cases_halfway - halfway_old$inmates
